Fixed re-shuffling between games; Fixed calculation of payoffs and display in results screen

centipede_SN/models.py
@@ -29,64 +29,104 @@ class Constants(BaseConstants):
     num_games = 3
     num_rounds = num_nodes * num_games
 
-    large_pile = c(0.40)
-    small_pile = c(0.10)
+    first_rounds = np.arange(1,num_rounds,num_nodes)
+    last_rounds = np.arange(num_nodes,num_rounds+1,num_nodes)
+
+    large_pile = 0.40
+    small_pile = 0.10
     base = 2
+
+    large_piles = []
+    small_piles = []
+
+    for node in range(num_nodes + 1):
+        large_piles.append(large_pile * base ** node)
+        small_piles.append(small_pile * base ** node)
 
 class Subsession(BaseSubsession):
 
+    game = models.IntegerField(initial=1)
+    game_node = models.IntegerField(initial=1)
+
     def creating_session(self):
-        if self.round_number == 1:
 
-            #self.session.vars['game_off_num'] = 0
-            self.session.vars['large_piles'] = []
-            self.session.vars['small_piles'] = []
+        for x in range(1,Constants.num_rounds+1):
+            self.in_round(x).game = np.ceil(x/Constants.num_nodes)
+            self.in_round(x).game_node = x - (np.ceil(x/Constants.num_nodes)-1)*Constants.num_nodes
 
-            for node in range(Constants.num_nodes + 1):
-                self.session.vars['large_piles'].append(c(Constants.large_pile * Constants.base ** node))
-                self.session.vars['small_piles'].append(c(Constants.small_pile * Constants.base ** node))
+        # shuffles players in groups randomly
+        if self.round_number in Constants.first_rounds:
+            self.group_randomly()
+        else:
+            x = Constants.first_rounds[int(np.ceil(self.round_number / Constants.num_nodes)) - 1]
+            self.group_like_round(x)
 
-            for p in self.get_players():
-                p.participant.vars['game'] = 1
-                p.participant.vars['game_node'] = 1
+        # shuffles players in groups randomly but keep players in fixed roles
+        #if self.round_number in Constants.first_rounds:
+        #    self.group_randomly(fixed_id_in_group=True)
+        #else:
+        #    x = Constants.first_rounds[int(np.ceil(self.round_number / Constants.num_nodes)) - 1]
+        #    self.group_like_round(x)
+
+        # shuffles players in groups as specified in matrix
+        #if self.round_number == Constants.first_rounds[0]:
+        #    new_structure = [[1, 2], [3, 4]]
+        #    self.set_group_matrix(new_structure)
+        #elif self.round_number == Constants.first_rounds[1]:
+        #    new_structure = [[1, 3], [2, 4]]
+        #    self.set_group_matrix(new_structure)
+        #elif self.round_number == Constants.first_rounds[2]:
+        #    new_structure = [[1, 4], [3, 2]]
+        #    self.set_group_matrix(new_structure)
+        #else:
+        #    x = Constants.first_rounds[int(np.ceil(self.round_number / Constants.num_nodes)) - 1]
+        #    self.group_like_round(x)
 
     def advance_game(self):
-        print(self.get_group_matrix())
-        new_structure = [[4, 1], [2, 3]]
-        self.set_group_matrix(new_structure)
-        print(self.get_group_matrix())
-
-        for p in self.get_players():
-            if p.participant.vars['game'] < Constants.num_games:
-                p.participant.vars['game'] += 1
-                p.participant.vars['game_node'] = 1
         for g in self.get_groups():
-            g.game_on = True
+            if g.subsession.game < Constants.num_games:
+                g.in_round(g.round_number + 1).game_on = True
 
 class Group(BaseGroup):
 
     game_on = models.BooleanField(initial=True)
+    game_outcome = models.IntegerField(initial=0)
+    last_node = models.IntegerField(initial=1)
 
-    def stop_game_or_advance_node(self):
-        for p in self.get_players():
-            if p.take or p.participant.vars['game_node'] == Constants.num_nodes:
-                self.game_on = False
-                #self.session.vars['game_off_num'] += 1
-            else:
-                p.participant.vars['game_node'] += 1
+    def stop_game(self):
+        players = self.get_players()
+        takes = [p.take for p in players]
+        if takes[0]:
+            self.game_on = False
+            self.game_outcome = 1
+            self.last_node = self.subsession.game_node
+        elif takes[1]:
+            self.game_on = False
+            self.game_outcome = 2
+            self.last_node = self.subsession.game_node
+        elif self.subsession.game_node == Constants.num_nodes and not any(takes):
+            self.game_on = False
+            self.last_node= self.subsession.game_node
+
+        for group in self.in_rounds(self.round_number + 1, Constants.last_rounds[self.subsession.game-1]):
+            group.game_on = self.in_round(self.round_number).game_on
+            group.game_outcome = self.in_round(self.round_number).game_outcome
+            group.last_node = self.in_round(self.round_number).last_node
 
     def set_payoffs(self):
+        players = self.get_players()
+        takes = [p.take for p in players]
         for p in self.get_players():
-            if p.participant.vars['game_node'] == Constants.num_nodes:
+            if self.subsession.game_node == Constants.num_nodes and not any(takes):
                 if p.id_in_group == 1:
-                    p.payoff = p.session.vars['large_piles'][-1]
+                    p.payoff = c(Constants.large_piles[-1])
                 else:
-                    p.payoff = p.session.vars['small_piles'][-1]
-            elif not self.game_on:
+                    p.payoff = Constants.small_piles[-1]
+            elif self.subsession.game_node < Constants.num_nodes and any(takes):
                 if p.take:
-                    p.payoff = self.session.vars['large_piles'][p.participant.vars['game_node'] - 1]
+                    p.payoff = c(Constants.large_piles[self.last_node - 1])
                 else:
-                    p.payoff = self.session.vars['small_piles'][p.participant.vars['game_node'] - 1]
+                    p.payoff = c(Constants.small_piles[self.last_node - 1])
 
 class Player(BasePlayer):
 