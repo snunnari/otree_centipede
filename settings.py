55ac7ae7f3cc7ae5c98fc9050b80ee2a1b080797
snunnari <49909509+snunnari@users.noreply.github.com>
2021-06-15 10:56:53 +0200

    Update room settings

--- settings.py
+++ settings.py
@@ -28,7 +28,53 @@ LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'EUR'
 USE_POINTS = False
 
-ROOMS = []
+ROOMS = [
+dict(
+    name='Prolific_1',
+    display_name='Prolific_1',
+    use_secure_urls=False
+),
+dict(
+    name='Prolific_2',
+    display_name='Prolific_2',
+    use_secure_urls=False
+),
+dict(
+    name='Prolific_3',
+    display_name='Prolific_3',
+    use_secure_urls=False
+),
+dict(
+    name='SONA_1',
+    display_name='SONA_1',
+    use_secure_urls=False
+),
+dict(
+    name='SONA_2',
+    display_name='SONA_2',
+    use_secure_urls=False
+),
+dict(
+    name='SONA_3',
+    display_name='SONA_3',
+    use_secure_urls=False
+),
+dict(
+    name='BELSS',
+    display_name='BELSS',
+    participant_label_file='_rooms/belss.txt',
+    use_secure_urls=True
+),
+dict(
+    name='30463',
+    display_name='Intro to Cognitive Science',
+    use_secure_urls=False
+),
+dict(
+    name='live_demo',
+    display_name='Room for Live Demo (No Participant Labels)',
+)
+]
 
 ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
